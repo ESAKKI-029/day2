6118a1ce03c64071549a8e31dede7bb292a5fa2e
ESAKKI-029 <108513721+ESAKKI-029@users.noreply.github.com>
2023-04-03 09:08:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './comp/Header';
+function App() {
+  return (
+    <div>
+     <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/comp/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { AppBar } from '@mui/material'
+import Toolbar from '@mui/material'
+export default function Header() {
+  return (
+    <div>
+        <AppBar color="primary" position="static">
+        <h1> My Store</h1>
+         </AppBar>
+    </div>
+  )
+}
